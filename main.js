d009a4fa02e99cead3b0c6f939e046a1a05b44fa
Bennett Yeagley <46436812+byeagley@users.noreply.github.com>
2025-01-27 17:37:50 -0600

    style based on mobile by default

---
+++ main.js
@@ -0,0 +1,11 @@
+const primaryHeader = document.querySelector(".primary-header");
+const scrollWatcher = document.createElement("div");
+
+scrollWatcher.setAttribute("data-scroll-watcher", "");
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+  primaryHeader.classList.toggle("sticking", !entries[0].isIntersecting);
+});
+
+navObserver.observe(scrollWatcher);
